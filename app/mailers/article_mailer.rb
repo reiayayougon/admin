3ee8bc15645e249681f98c1d776badb34bcfb967
reiayayougon <ishida.ayako0306@gmail.com>
2023-06-20 15:01:49 +0900

    インデント調整インデント調整

--- app/mailers/article_mailer.rb
+++ app/mailers/article_mailer.rb
@@ -1,7 +1,7 @@
 class ArticleMailer < ApplicationMailer
-    def report_summary
-        @published_article_count = Article.published.count
-        @articles_published_at_yesterday = Article.published_at_yesterday
-        mail(to: 'admin@example.com', subject: '公開済記事の集計結果')
-    end
+  def report_summary
+    @published_article_count = Article.published.count
+    @articles_published_at_yesterday = Article.published_at_yesterday
+    mail(to: 'admin@example.com', subject: '公開済記事の集計結果')
+  end
 end
